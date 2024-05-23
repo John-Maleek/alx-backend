2d2e240e0c29bada4e958813680449901905e2a7
John Maleek <jmzoran01@gmail.com>
2024-05-23 04:25:04 +0100

    first commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+    module defines a basic cache
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache module:
+      Inherits from the BaseCaching module
+    """
+    def __init__(self):
+        """
+        initialize class instance
+        """
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """
+        add an item to the caching system
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Returns: item associated with the key
+        None if the key is not in the cache
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
